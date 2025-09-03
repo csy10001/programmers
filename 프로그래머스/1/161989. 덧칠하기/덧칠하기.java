ecf5c7eae60de6417395cdd1d9c2c62a728e28c7
csy10001 <fuitshine@gmail.com>
2025-09-03 09:46:58 +0900

    [level 1] Title: 덧칠하기, Time: 2.20 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int paintedUntil = 0;
+        
+        for (int s : section) {
+            if (s <= paintedUntil) continue;
+
+            answer++;
+            paintedUntil = s + m - 1;
+        }
+        return answer;
+    }
+}
