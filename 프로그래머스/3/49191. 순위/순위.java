1c50af48117a0d818f68f49c01858362d7792950
csy10001 <fuitshine@gmail.com>
2025-12-08 20:32:32 +0900

    [level 3] Title: 순위, Time: 13.30 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int solution(int n, int[][] results) {
+        boolean[][] win = new boolean[n + 1][n + 1];
+        boolean[][] lose = new boolean[n + 1][n + 1];
+        
+        for (int[] result : results) {
+            int winner = result[0];
+            int loser = result[1];
+            win[winner][loser] = true;
+            lose[loser][winner] = true;
+        }
+        
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (win[i][k] && win[k][j]) {
+                        win[i][j] = true;
+                        lose[j][i] = true;
+                    }
+                }
+            }
+        }
+        int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = 1; j <= n; j++) {
+                if (i == j) continue;
+
+                if (win[i][j] || lose[i][j]) {
+                    count++;
+                }
+            }
+            if (count == n - 1) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
